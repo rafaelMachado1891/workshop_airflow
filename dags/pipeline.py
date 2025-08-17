ed56d25a0151468d97419feee0dd09b1d4143cf3
rafaelMachado1891 <rafadmn1@gmail.com>
2025-08-17 16:00:59 -0300

    update

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -2,7 +2,6 @@ from time import sleep
 from airflow.decorators import dag, task
 from datetime import datetime
 
-
 @dag(
         dag_id="minha_pipeline",
         description="minha etl",
@@ -12,23 +11,31 @@ from datetime import datetime
 )
 def pipeline():
 
+    @task
     def minha_primeira_atividade():
         print("minha_primeira_atividade")
         sleep(5)
 
+    @task
     def minha_segunda_atividade():
         print("minha_segunda_atividade")
         sleep(5)
 
+    @task
     def minha_terceira_atividade():
         print("minha_terceira_atividade")
         sleep(5)
 
+    @task
     def minha_quarta_atividade():
         print("Pipeline Finalizou!")
 
-    def pipeline():
-        minha_primeira_atividade()
-        minha_segunda_atividade()
-        minha_terceira_atividade()
-        minha_quarta_atividade()
+    t1 = minha_primeira_atividade()
+    t2 = minha_segunda_atividade()
+    t3 = minha_terceira_atividade()
+    t4 = minha_quarta_atividade()
+
+    t1 >> t2 >> t3 >> t4  # dependências
+
+pipeline()
+
